a0e7054438506f18ce9469b1dde6ab2bd464e3ab
arrudoprogramador <joaopedroarruda323@gmail.com>
2026-08-22 14:13:21 -0300

    feat: resolução exercício 5

--- Lote 01/EstSeq05.java
+++ Lote 01/EstSeq05.java
@@ -1,5 +1,20 @@
 import javax.swing.*;
 
 public class EstSeq05 {
-    
+	public static void main(String[] args) {
+		Double a = 0.0, b = 0.0, c = 0.0, delta,  raiz1, raiz2;
+		
+		a = Double.parseDouble(JOptionPane.showInputDialog("Digite os graus Celsius: "));
+		b = Double.parseDouble(JOptionPane.showInputDialog("Digite os graus Celsius: "));
+		c = Double.parseDouble(JOptionPane.showInputDialog("Digite os graus Celsius: "));
+
+		delta = ((b * b) - 4 * a * c);
+		
+		raiz1 = ((-b) + Math.sqrt(delta)) /(2 * a);
+		raiz2 = ((-b) - Math.sqrt(delta)) /(2 * a);
+		
+		JOptionPane.showMessageDialog(null,
+			    String.format("Raiz 1: %.2f%nRaiz 2: %.2f", raiz1, raiz2),
+			    "Resultado da Equação",
+			    JOptionPane.INFORMATION_MESSAGE);	}
 }

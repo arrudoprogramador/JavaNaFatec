b1c5e4194fb4125ec2f324bd9c3a309be11819f5
arrudoprogramador <joaopedroarruda323@gmail.com>
2026-08-22 16:10:28 -0300

    feat: resolução exercício 8

--- Lote 01/EstSeq08.java
+++ Lote 01/EstSeq08.java
@@ -1,5 +1,15 @@
 import javax.swing.*;
 
 public class EstSeq08 {
-    
+	
+	public static void main(String[] args) {
+		
+		Double depositoPoupanca = 0.0, valorPosterior = 0.0;
+		
+		depositoPoupanca = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do depósito em poupança: "));
+		
+		valorPosterior = depositoPoupanca * 1.3;
+		
+		JOptionPane.showMessageDialog(null, "Valor após 1 mês de aplicação: R$" + valorPosterior);	
+	}
 }

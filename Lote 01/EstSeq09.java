28a6964ee59a447e7d7c3364e9c49e22d047afd7
arrudoprogramador <joaopedroarruda323@gmail.com>
2026-08-22 14:29:03 -0300

    feat: resolução exercício 9

--- Lote 01/EstSeq09.java
+++ Lote 01/EstSeq09.java
@@ -1,5 +1,15 @@
 import javax.swing.*;
 
 public class EstSeq09 {
-    
+	public static void main(String[] args) {
+		int numero1 = 0, numero2 = 0, somaQuadrados = 0;
+		
+		numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número: "));
+		numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número: "));
+
+		somaQuadrados = (numero1 * numero1) + (numero2 * numero2);
+
+		
+		JOptionPane.showMessageDialog(null, "Soma dos quadrados: " + somaQuadrados);	
+	}
 }

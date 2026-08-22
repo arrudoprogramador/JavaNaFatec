99f998f9f1e5ef9eb9337cbda0c2d6759871d066
arrudoprogramador <joaopedroarruda323@gmail.com>
2026-08-22 14:50:42 -0300

    feat: resolução exercício 6

--- Lote 01/EstSeq06.java
+++ Lote 01/EstSeq06.java
@@ -1,5 +1,17 @@
 import javax.swing.*;
 
 public class EstSeq06 {
-    
+	public static void main(String[] args) {
+		int x = 0, y = 0, coringa;
+		
+		x = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor de X: "));
+		y = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor de Y: "));
+
+		
+		coringa = x;
+		x = y;
+		y = coringa;
+		
+		JOptionPane.showMessageDialog(null, "Valor de X: " + x + "\nValor de Y: " + y);	
+	}
 }

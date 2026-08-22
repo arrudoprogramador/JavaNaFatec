63c56928c8012488825ca647249e56ba9aa1e7ad
arrudoprogramador <joaopedroarruda323@gmail.com>
2026-08-22 13:12:05 -0300

    feat: resolução exercício 3

--- Lote 01/EstSeq03.java
+++ Lote 01/EstSeq03.java
@@ -1,5 +1,14 @@
 import javax.swing.*;
 
 public class EstSeq03 {
-    
+	public static void main(String[] args) {
+		Double baseTriangulo = 0.0, alturaTriangulo = 0.0, areaTriangulo = 0.0;
+		
+		baseTriangulo = Double.parseDouble(JOptionPane.showInputDialog("Digite a base do triângulo: "));
+		alturaTriangulo = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura do triângulo: "));
+	
+		areaTriangulo = (alturaTriangulo * baseTriangulo)/2;
+		
+		JOptionPane.showMessageDialog(null, "A área do triangulo é de: " + areaTriangulo);
+	}
 }

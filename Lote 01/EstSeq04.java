0991c95898f7a88fb45bba6ec10e50177f131293
arrudoprogramador <joaopedroarruda323@gmail.com>
2026-08-22 13:15:27 -0300

    feat: resolução exercício 4

--- Lote 01/EstSeq04.java
+++ Lote 01/EstSeq04.java
@@ -1,5 +1,13 @@
 import javax.swing.*;
 
 public class EstSeq04 {
-    
+	public static void main(String[] args) {
+		Double grausCelsius = 0.0, grausFahrenheit = 0.0;
+		
+		grausCelsius = Double.parseDouble(JOptionPane.showInputDialog("Digite os graus Celsius: "));
+	
+		grausFahrenheit = (9 * grausCelsius + 160)/5;
+		
+		JOptionPane.showMessageDialog(null, "A temperatura em graus Fahrenheit é de: " + grausFahrenheit);
+	}
 }

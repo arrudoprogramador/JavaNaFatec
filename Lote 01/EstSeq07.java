9afcfb5b21985e98cc3b0d2348f2f0d035e8beac
arrudoprogramador <joaopedroarruda323@gmail.com>
2026-08-22 14:25:01 -0300

    feat: resolução exercício 7

--- Lote 01/EstSeq07.java
+++ Lote 01/EstSeq07.java
@@ -1,5 +1,16 @@
 import javax.swing.*;
 
 public class EstSeq07 {
-    
+	public static void main(String[] args) {
+		Double comprimento = 0.0, largura = 0.0, altura = 0.0, volume = 0.0;
+		
+		comprimento = Double.parseDouble(JOptionPane.showInputDialog("Digite comprimento: "));
+		largura = Double.parseDouble(JOptionPane.showInputDialog("Digite largura: "));
+		altura = Double.parseDouble(JOptionPane.showInputDialog("Digite altura: "));
+
+		volume = comprimento * largura * altura;
+
+		
+		JOptionPane.showMessageDialog(null, "Volume: " + volume);	
+	}
 }
